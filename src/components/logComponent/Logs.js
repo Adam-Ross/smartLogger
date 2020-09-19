234bbd352a753bb9c017697f8e22edf7c381214f
Adam Ross <adamgarrettross@gmail.com>
2020-09-18 21:37:56 -0500

    Building out logs and title component.

---
+++ src/components/logComponent/Logs.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./logs.css";
+
+const Logs = () => {
+  return (
+    <Fragment>
+      <h1 id="logsTitle">LOGS</h1>
+    </Fragment>
+  );
+};
+
+export default Logs;
---
+++ src/components/titleComponent/Title.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import "./title.css";
+
+const Title = () => {
+  return (
+    <Fragment>
+      <h1 id="titleLarge">Smart Log</h1>
+      <p id="titleSecondary">Sensible logging for your business</p>
+    </Fragment>
+  );
+};
+
+export default Title;
