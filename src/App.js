fa0896b27908dcd9c797b3c2c2d591d5f0927b6b
Adam Ross <adamgarrettross@gmail.com>
2020-09-21 12:37:34 -0500

    Added materialize - there are some problems with it, but will fix them later... Or better yet, just not use it in any more projects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { Fragment } from "react";
-import Logs from "./components/logComponent/Logs";
-import Title from "./components/titleComponent/Title";
+import React, { Fragment, useEffect } from "react";
+// import Logs from "./components/logComponent/Logs";
+// import Title from "./components/titleComponent/Title";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
 const App = () => {
+  useEffect(() => {
+    // Initializes materialize JS... Modals
+    M.AutoInit();
+  });
+
   return (
     <Fragment>
-      <div className="container">
-        <Title />
-        <Logs />
-      </div>
+      <h1>This is the thing.</h1>
     </Fragment>
   );
 };
--- src/components/logComponent/Logs.js
+++ src/components/logComponent/Logs.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import "./logs.css";
 
 const Logs = () => {
   return (
